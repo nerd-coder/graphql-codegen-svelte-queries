d903d812b43b30a0d8fb0390f6475d3c64ff12ae
Toàn <me@toan.io>
2023-06-10 01:23:02 +0700

    feat: add rawData

--- src/index.ts
+++ src/index.ts
@@ -90,8 +90,14 @@ export const plugin: PluginFunction<SvelteApolloPluginConfig, Types.ComplexPlugi
   ]
   const helpers = [
     `
-type ReadableQueryResult<T, V extends OperationVariables> = {
+type ReadableQueryResult<
+  T,
+  V extends OperationVariables,
+  K extends Exclude<keyof T, '__typename'>
+> = {
   readonly query: ObservableQuery<T, V>
+  readonly errorMessages: Readable<string[]>
+  readonly rawData: Readable<T[K] | null>
 } & Required<{
   readonly [P in keyof ApolloQueryResult<T>]: Readable<
     P extends 'data' ? T | null : ApolloQueryResult<T>[P]
@@ -99,10 +105,11 @@ type ReadableQueryResult<T, V extends OperationVariables> = {
 }>`,
     `type ReadableQueryOption<T, V extends OperationVariables> = Omit<WatchQueryOptions<V, T>, 'query'>`,
     `
-function __buildReadableQuery<T, V extends OperationVariables>(
-  doc: DocumentNode,
-  options?: ReadableQueryOption<T, V>
-): ReadableQueryResult<T, V> {
+function __buildReadableQuery<
+  T,
+  V extends OperationVariables,
+  K extends Exclude<keyof T, '__typename'>
+>(doc: DocumentNode, options?: ReadableQueryOption<T, V>): ReadableQueryResult<T, V, K> {
   const query = client.watchQuery({ query: doc, ...options })
   const result = readable<ApolloQueryResult<T | null>>(
     { data: null, loading: true, networkStatus: NetworkStatus.loading },
@@ -114,12 +121,22 @@ function __buildReadableQuery<T, V extends OperationVariables>(
     }
   )
   const data = derived(result, r => r.data)
+  const rawData = derived(result, r => {
+    if (!r.data) return null
+    const k = Object.keys(r.data).filter(z => z !== '__typename')[0] as K
+    if (!k) return null
+    return r.data[k]
+  })
   const error = derived(result, r => r.error)
   const errors = derived(result, r => r.errors)
   const loading = derived(result, r => r.loading)
   const networkStatus = derived(result, r => r.networkStatus)
   const partial = derived(result, r => r.partial)
-  return { query, data, error, errors, loading, networkStatus, partial }
+  const errorMessages = derived(result, q => [
+    ...(q.error ? [q.error.message] : []),
+    ...(q.errors ? q.errors.map(z => z.message) : []),
+  ])
+  return { query, data, error, errors, loading, networkStatus, partial, errorMessages, rawData }
 }`,
   ]
 
